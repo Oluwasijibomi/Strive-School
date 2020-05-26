99d5b2b8154d7fe7b23e781f7c1e682f96b1641a
Shiji.js <hafeetech@gmail.com>
2020-05-26 08:24:36 +0200

    Update script.js
    Working on Homework

--- M3-Advanced-JS/week-1/Day-2/script.js
+++ M3-Advanced-JS/week-1/Day-2/script.js
@@ -1,3 +1,5 @@
+window.onload = function() {
+
 fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem", {
 	"method": "GET",
 	"headers": {
@@ -5,25 +7,39 @@ fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem", {
 		"x-rapidapi-key": "46b7b8bb48msh18118826d934364p1f8e03jsn798d9c136d1a"
 	}
 })
-.then(response => response.json())
-.then(convertedApiResponse => {
-  console.log(convertedApiResponse)
+
+  .then(response => response.json())
+  .then(parsedJson => {
+    console.log(parsedJson)
+
+    let albumsDiv = document.querySelector("#albums")
 
 
-let songs = convertedApiResponse.data;
-
-let songName = songs[7].title;
-console.log(songName);
-
-for (i = 0; i < songs.length; i++ ){
-  let song = songs[i]
-
-  console.log(song.title);
-};
-})
+    let songs = parsedJson.data;
+    console.log(songs)
 
 
+    for (let i = 0; i < songs.length; i++) {
 
-.catch(err => {
-	console.log(err);
-});
+      let song = songs[i]
+      //console.log(songs[i].title)
+      albumsDiv.innerHTML =+ 
+      `<div class="card col-sm-3">
+        <img src="${song.album.cover_big}"class="card-img-top" alt=".."/>
+        <div class="card-body">
+          <h5 class="card-title">${song.title}</h5>
+          <p class="card-text">${song.artist.name} - ${song.album.title} </p>
+        </div>
+      </div>`
+      
+
+        
+    }
+  })
+
+
+
+
+
+
+}
